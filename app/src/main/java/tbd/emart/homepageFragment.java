8b10ceefcffcfc59c8ab58c7c8aaa2d8c5a399cf
xianchizou <xianchi.zou@gmail.com>
2016-09-20 19:59:27 -0400

    Bar buttons implementation3

---
+++ app/src/main/java/tbd/emart/homepageFragment.java
@@ -0,0 +1,21 @@
+package tbd.emart;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kehong Yi on 9/19/2016.
+ */
+public class homepageFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.drawer_homepage, container, false);
+        return myView;
+    }
+}
